5e6f18a2f5955c7e09a0ea65dce90de97d4bc5a0
Benson Tsai <btsai@vrwarp.com>
2026-06-06 19:02:36 -0700

    chore: ignore scratch/ dir

--- scratch/check_lowercase_caron.py
+++
@@ -1,6 +0,0 @@
-from fontTools.ttLib import TTFont
-font = TTFont('resources/fonts/DroidSansFallbackFull.ttf')
-cmap = font.getBestCmap()
-for char in ['ǎ', 'ǐ', 'ǒ', 'ǔ', 'ǚ']:
-    cp = ord(char)
-    print(f"{char} (U+{cp:04X}): {'In cmap' if cp in cmap else 'NOT in cmap'}")
--- scratch/inspect_glyph.py
+++
@@ -1,7 +0,0 @@
-from fontTools.ttLib import TTFont
-font = TTFont("resources/fonts/PT_Sans-Narrow-Web-Regular.ttf")
-glyf = font['glyf']
-g = glyf['A']
-print("Attributes:", dir(g))
-if hasattr(g, 'xMin'):
-    print(f"BBox: {g.xMin}, {g.yMin}, {g.xMax}, {g.yMax}")
